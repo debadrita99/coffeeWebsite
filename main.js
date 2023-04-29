45a1724a7f334c919b23663c2b0f2857f61a3c16
Debadrita Ghosh <96650472+debadrita99@users.noreply.github.com>
2023-04-29 14:05:07 +0530

    added index.html

---
+++ main.js
@@ -0,0 +1,22 @@
+let searchBox = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () =>{
+    searchBox.classList.toggle('active');
+    navbar.classList.remove('active');
+    
+}
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchBox.classList.remove('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+    searchBox.classList.remove('active');
+}
+
+
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
